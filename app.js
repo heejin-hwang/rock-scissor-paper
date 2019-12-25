cad1527cfcabf2c6d53e079195e4515b6422911c
heejin-hwang <heyday.xz@gmail.com>
2019-12-25 22:06:51 +0900

    rock, scissor, paper game with computer

---
+++ app.js
@@ -0,0 +1,134 @@
+new Vue({
+    el: '#app',
+    data: {
+        myChoice: null,
+        comChoice: null,
+        count: 3,
+        winner: null,
+        lifeOfMe: 3,
+        lifeOfCom: 3,
+        isSelectable: true,
+        logs: [],
+        selects: [
+            { name: '가위', value: 'scissor' },
+            { name: '바위', value: 'rock' },
+            { name: '보', value: 'paper' }
+
+        ]
+    },
+    computed: {
+        myChoiceImg: function() {
+            return this.myChoice !== null ? `images/${this.myChoice}.jpg` : 'images/question.jpg';
+        },
+        comChoiceImg: function() {
+            return this.comChoice !== null ? `images/${this.comChoice}.jpg` : 'images/question.jpg';
+        },
+        leftLifeOfMe: function() {
+            return 3 - this.lifeOfMe;
+        },
+        leftLifeOfCom: function() {
+            return 3 - this.lifeOfMe;
+        }
+    },
+    watch: {
+        count: function(newVal) {
+            if (newVal === 0) {
+                // 컴퓨터가 가위바위보를 선택하는 부분
+                this.selectCom();
+
+                // 가위바위보 승패 결정 & 몫을 차감
+                this.whoIsWin();
+
+
+                // 게임 리셋
+                this.count = 3;
+                this.isSelectable = true;
+
+                // 로그를 업데이트 하는 부분
+                this.updateLogs();
+            }
+
+        },
+        lifeOfMe: function(newVal) {
+            if (newVal === 0) {
+                // 게임을 종료
+                this.endGame('안타깝네요. 당신이 패배하였습니다.');
+            }
+        },
+        lifeOfCom: function(newVal) {
+            if (newVal === 0) {
+                // 게임을 종료
+                this.endGame('축하드립니다. 당신이 승리하였습니다.');
+            }
+        }
+    },
+    methods: {
+        startGame: function() {
+
+            if (this.myChoice === null) {
+                alert('가위 바위 보 중 하나를 선택해주세요.');
+            } else {
+                // 버튼이 보이지 않음
+                this.isSelectable = false;
+                // 이 시간이 지날 떄마다 이 기능을 실행시켜줘
+                //콜백함수가  this  의 위치를 변환시키기 때문에 애로우 펑션 사용
+                let countDown = setInterval(() => {
+                    // 셋인터벌로 일초에 한번씩 이 함수를 실행할건데
+                    this.count--;
+                    if (this.count === 0) {
+                        // count가 0이라면 클리어인터벌, 인터벌 자체를 멈춰달라
+                        clearInterval(countDown);
+                    }
+                }, 1000);
+            }
+        },
+        selectCom: function() {
+
+            let number = Math.random();
+            if (number < 0.33) {
+                this.comChoice = 'scissor';
+            } else if (number < 0.66) {
+                this.comChoice = 'rock';
+            } else {
+                this.comChoice = 'paper';
+            }
+        },
+        whoIsWin: function() {
+            if (this.myChoice === this.comChoice) this.winner = 'no one';
+            else if (this.myChoice === 'rock' && this.comChoice === 'scissor') this.winner = 'me';
+            else if (this.myChoice === 'rock' && this.comChoice === 'paper') this.winner = 'com';
+            else if (this.myChoice === 'scissor' && this.comChoice === 'paper') this.winner = 'me';
+            else if (this.myChoice === 'scissor' && this.comChoice === 'rock') this.winner = 'com';
+            else if (this.myChoice === 'paper' && this.comChoice === 'rock') this.winner = 'me';
+            else if (this.myChoice === 'paper' && this.comChoice === 'scissor') this.winner = 'com';
+            else this.winner = 'error';
+
+            // 몫 차감
+            if (this.winner === 'me') {
+                this.lifeOfCom--;
+
+            } else if (this.winner === 'com') {
+                this.lifeOfMe--;
+            }
+        },
+        updateLogs: function() {
+            let log = {
+                message: `You: ${this.myChoice}, Computer: ${this.comChoice}`,
+                winner: this.winner
+
+            };
+            this.logs.unshift(log);
+        },
+        endGame: function(msg) {
+            setTimeout(() => {
+                confirm(msg);
+                this.lifeOfMe = 3;
+                this.lifeOfCom = 3;
+                this.myChoice = null;
+                this.comChoice = null;
+                this.winner = winner;
+                this.log = [];
+            }, 500);
+        }
+    },
+});
